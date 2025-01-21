17cd435427b21ac40b69270d7f470cef00d8e413
ECKERT RECKTENVALD Bernardo <beckertreck@cloche.pedago.ipb.fr>
2025-01-21 11:40:58 +0100

    Polling du bouton

--- main.cpp
+++ main.cpp
@@ -5,22 +5,46 @@
 
 #include "mbed.h"
 
+#define BUTTON_PIN BUTTON1
+#define LED_PIN LED1
 
-// Blinking rate in milliseconds
-#define BLINKING_RATE     500ms
-
+#define MAXIMUM_BUFFER_SIZE  
+BufferedSerial serial_port(USBTX, USBRX);
 
 int main()
 {
     // Initialise the digital pin LED1 as an output
-#ifdef LED1
-    DigitalOut led(LED1);
-#else
-    bool led;
-#endif
+    #ifdef LED1
+        DigitalOut led(LED1);
+    #else
+        bool led;
+    #endif
+    // Initialise the digital pin BUTTON1 as an input
+    #ifdef LED1
+        DigitalIn button(BUTTON1);
+    #else
+        bool button;
+    #endif
+
+    // Set desired properties (9600-8-N-1).
+    serial_port.set_baud(9600);
+    serial_port.set_format(
+        /* bits */ 8,
+        /* parity */ BufferedSerial::None,
+        /* stop bit */ 1
+    );
+
+    // Application buffer to receive the data
+    uint32_t text;
 
     while (true) {
-        led = !led;
-        ThisThread::sleep_for(BLINKING_RATE);
+        if(button == 1){
+            led = 1;
+            serial_port.write("Button Pressed\n", 16);
+        } else {
+            led = 0;
+            serial_port.write("Button Released\n", 17);
+        }
+        ThisThread::sleep_for(100ms);
     }
 }
