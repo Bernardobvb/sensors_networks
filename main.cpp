f2971c59d12fd8f9c9aff2d0228319ab3f72dfb2
ECKERT RECKTENVALD Bernardo <beckertreck@cloche.pedago.ipb.fr>
2025-01-21 15:26:39 +0100

    Controle de la LED par un ticker

--- main.cpp
+++ main.cpp
@@ -6,29 +6,37 @@
 #include "mbed.h"
 
 #define BUTTON_PIN BUTTON1
-#define LED_PIN LED1
+#define LED1_PIN LED1
+#define LED2_PIN LED2
 
 int flag = 0;
 
-DigitalOut led(LED1);
+DigitalOut led1(LED1);
+DigitalOut led2(LED2);
 InterruptIn button(BUTTON1);
 
 BufferedSerial serial_port(USBTX, USBRX);
 
 Timer t;
+Ticker flipper;
 
 void led_on()
 {
     t.start();
-    led = 1;
+    led1 = 1;
     flag = 1;
 }
 
 void led_off()
 {
     t.stop();
-    led = 0;
+    led1 = 0;
     flag = 1;
+}
+
+void flip()
+{
+    led2 = !led2;
 }
 
 int main()
@@ -42,9 +50,10 @@ int main()
     );
     button.rise(&led_on);
     button.fall(&led_off);
+    flipper.attach(&flip, 2.0);
 
     while (true) {
-        if (led == 1)
+        if (led1 == 1)
         {
             if (flag == 1)
             {
