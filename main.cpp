c70ecc2eb60fcfa561381be005d72a16263cf431
ECKERT RECKTENVALD Bernardo <beckertreck@cloche.pedago.ipb.fr>
2025-01-21 17:15:44 +0100

    Communiquer avec le capteur

--- main.cpp
+++ main.cpp
@@ -4,6 +4,7 @@
  */
 
 #include "mbed.h"
+#include "htu21d/htu21d.h"
 
 #define BUTTON_PIN BUTTON1
 #define LED1_PIN LED1
@@ -20,6 +21,12 @@ BufferedSerial serial_port(USBTX, USBRX);
 
 Timer t;
 Ticker flipper;
+
+namespace {
+#define WAIT 1000ms
+}
+
+using namespace sixtron;
 
 void flip()
 {
@@ -45,6 +52,29 @@ void led_off()
 
 int main()
 {
+    HTU21D::ErrorType err;
+    htud21d_user_reg_t reg;
+    HTU21D sensor(P1_I2C_SDA, P1_I2C_SCL);
+    double result;
+
+    sensor.soft_reset();
+
+    printf("\n\n----------------------\n");
+    printf("--- HTU21D(F) Demo ---\n");
+
+    err = sensor.read_user_register(&reg);
+
+    if (err == HTU21D::ErrorType::Ok) {
+        printf("User register status:\n");
+        printf("Battery low: %s\nHeater: %s\nOTP reload: %s\n\n",
+                reg.end_of_battery ? "TRUE" : "FALSE",
+                reg.enable_heater ? "ON" : "OFF",
+                reg.disable_otp_reload ? "DISABLED" : "ENABLED");
+
+        reg.resolution = MeasurementResolution::RH_12_Temp_14;
+        sensor.write_user_register(&reg);
+    }
+
     // Set desired properties (9600-8-N-1).
     serial_port.set_baud(9600);
     serial_port.set_format(
@@ -56,6 +86,12 @@ int main()
     button.fall(&led_off);
 
     while (true) {
+        printf("-----\n");
+        err = sensor.read_humidity(&result);
+        if (err == HTU21D::ErrorType::Ok) {
+            printf("Relative humidity: %f%%\n", result);
+        }
+
         if (led1 == 1)
         {
             if (flag == 1)
